033f643bbfb8b50f90cc39d8e68c507e7524d0ee
AE Creations <aecreations@users.noreply.github.com>
2022-09-13 17:44:01 -0700

    Added menu items to the browser tab context menu

--- src/background.js
+++ src/background.js
@@ -135,13 +135,13 @@ async function setUICustomizations()
     browser.menus.create({
       id: "ae-readnext-add-bkmk",
       title: browser.i18n.getMessage("addBkmk"),
-      contexts: ["page"],
+      contexts: ["page", "tab"],
       visible: false,
     });
     browser.menus.create({
       id: "ae-readnext-submnu",
       title: browser.i18n.getMessage("extName"),
-      contexts: ["page"],
+      contexts: ["page", "tab"],
       visible: false,
     });
     browser.menus.create({
